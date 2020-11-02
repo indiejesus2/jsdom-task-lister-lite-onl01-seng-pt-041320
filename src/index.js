c7536138df29d1862457abb3a254a8ba77d2ec74
Dan Ste. Marie Jr <DSte.mariejr@gmail.com>
2020-11-02 13:38:02 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+const element = document.getElementById("create-task-form")
+const description = document.getElementById("new-task-description")
+let list = []
+let tasks = document.getElementById("tasks")
+
+
+function createList(list) {
+  tasks.innerHTML = ''
+  for (let i = 0; i < list.length; i++) {
+    let li = document.createElement("li");
+    li.innerText = list[i];
+    tasks.appendChild(li);
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  element.addEventListener('submit', function(event) {
+    event.preventDefault();
+    list.push(description.value);
+    event.target.reset();
+    createList(list);
+});
+
 });
